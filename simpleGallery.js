d295d91b7782edd6671d1ca58ef65aaf95a1944c
Anneke Sinnema <mail@annekesinnema.nl>
2016-07-20 18:13:59 +0200

    Fixed a bug that enabled navigating past the last slide, made script reusable

--- simpleGallery.js
+++ simpleGallery.js
@@ -6,98 +6,113 @@
  * Released under the MIT license
  */
 
-var steps = document.querySelectorAll('.process__step');
-var totalSteps = steps.length;
-var currentStep = 0;
+ function simpleGallery(liClassName) {
 
-showCurrentStep();
+	var steps = document.querySelectorAll(liClassName);
+	var totalSteps = steps.length;
+	var currentStep = 0;
+	var lastStep = steps.length - 1; 
 
-function showCurrentStep() {
-		var statusSpan = document.getElementById('js-stepStatus');
+	/** adds a little status bar with the current step **/
+	showCurrentStep();
 
-		var statusText = "Stap " + (currentStep+1) + " van " + totalSteps;
-		statusSpan.innerHTML = statusText;
+	function showCurrentStep() {
+		 var statusSpan = document.getElementById('js-stepStatus');
+
+		 var statusText = "Stap " + (currentStep+1) + " van " + totalSteps;
+		 statusSpan.innerHTML = statusText;
+	}
+
+	function goToFirst() {
+		 steps[currentStep].className = 'process__step';
+		 currentStep = 0;
+		 steps[currentStep].className = 'process__step process__step--active';
+
+		 setVisibility(triggerPrevious, false);
+		 setVisibility(triggerFirst, false);
+		 showCurrentStep();
+	}
+
+	function goToPrevious() {
+		 if (currentStep > 0) {
+				 steps[currentStep].className = 'process__step';
+				 currentStep = (currentStep-1);
+				 steps[currentStep].className = 'process__step process__step--active';
+				 setVisibility(triggerNext, true);
+				 showCurrentStep();
+		 }
+		 else if (currentStep == 0) {
+				 setVisibility(triggerFirst, false);
+				 setVisibility(triggerPrevious, false);
+		 }
+	}
+
+	function goToNext() {
+		 if (currentStep < lastStep) {
+				 steps[currentStep].className = 'process__step';
+				 currentStep = (currentStep+1);
+				 steps[currentStep].className = 'process__step process__step--active';
+				 showCurrentStep();
+		 }
+
+		 if (currentStep > 0) {
+				 setVisibility(triggerFirst, true);
+				 setVisibility(triggerPrevious, true);
+		 }
+
+		 if (currentStep == lastStep) {
+				 setVisibility(triggerNext, false);
+		 }
+	}
+
+	function setVisibility(trigger, doSetVisible) {
+		 trigger.className = doSetVisible ? 'is-visible' : 'is-hidden';
+		 trigger.setAttribute("aria-hidden", !doSetVisible);
+	}
+
+	var triggerFirst = document.getElementById('js-first');
+	var triggerPrevious = document.getElementById('js-previous');
+	var triggerNext = document.getElementById('js-next');
+
+	triggerFirst.addEventListener('click', function() {
+	 console.log('click registered: triggerfirst');
+		 goToFirst();
+		 event.preventDefault();
+	});
+
+	triggerNext.addEventListener('click', function() {
+	 console.log('click registered: next');
+		 goToNext();
+		 event.preventDefault();
+	});
+
+	triggerPrevious.addEventListener('click', function() {
+	 console.log('click registered: previous');
+		 goToPrevious();
+		 event.preventDefault();
+	});
+
+	document.onkeydown = checkKey;
+
+	function checkKey(e) {
+		 e = e || window.event;
+
+		 if (e.keyCode == '37') {
+				 goToPrevious();
+		 }
+		 else if (e.keyCode == '39') {
+				 goToNext();
+		 }
+	}
 }
 
-function goToFirst() {
-		steps[currentStep].className = 'process__step';
-		currentStep = 0;
-		steps[currentStep].className = 'process__step process__step--active';
+/** TL:DR; enter the tag containing text within quotation marks,
+add a comma and indicate after how many words you want to insert the break.
 
-		setVisibility(triggerPrevious, false);
-		setVisibility(triggerFirst, false);
-		showCurrentStep();
-}
+For example:
 
-function goToPrevious() {
-    if (currentStep > 0) {
-        steps[currentStep].className = 'process__step';
-        currentStep = (currentStep-1);
-        steps[currentStep].className = 'process__step process__step--active';
-				setVisibility(triggerNext, true);
-        showCurrentStep();
-    }
-		if (currentStep == 0) {
-				/** if this step is the first one **/
-				setVisibility(triggerFirst, false);
-				setVisibility(triggerPrevious, false);
-		}
-}
+<script>
+simpleGallery('.process__step');
+</script>
 
-function goToNext() {
-		if (currentStep < totalSteps) {
-				/** if this step is not the last one, go to the next step **/
-				steps[currentStep].className = 'process__step';
-				currentStep = (currentStep+1);
-				steps[currentStep].className = 'process__step process__step--active';
-				showCurrentStep();
-		}
-
-		if (currentStep > 0) {
-				/** if this step is not the first one, show the button to previous step **/
-				setVisibility(triggerFirst, true);
-				setVisibility(triggerPrevious, true);
-		}
-
-		if (currentStep == 2) {
-				/** if this step is the last one **/
-				setVisibility(triggerNext, false);
-		}
-}
-
-function setVisibility(trigger, doSetVisible) {
-	  trigger.className = doSetVisible ? 'is-visible' : 'is-hidden';
-	  trigger.setAttribute("aria-hidden", !doSetVisible);
-}
-
-var triggerFirst = document.getElementById('js-first');
-var triggerPrevious = document.getElementById('js-previous');
-var triggerNext = document.getElementById('js-next');
-
-triggerFirst.addEventListener('click', function() {
-		goToFirst();
-	  event.preventDefault();
-});
-
-triggerNext.addEventListener('click', function() {
-		goToNext();
-    event.preventDefault();
-});
-
-triggerPrevious.addEventListener('click', function() {
-		goToPrevious();
-		event.preventDefault();
-});
-
-document.onkeydown = checkKey;
-
-function checkKey(e) {
-    e = e || window.event;
-
-    if (e.keyCode == '37') {
-        goToPrevious();
-    }
-    else if (e.keyCode == '39') {
-				goToNext();
-    }
-}
+**/
